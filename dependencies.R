0615d660bfda7008d4120a1ca3b8fd9add86357f
Chris Davey <cd@cpd.id.au>
2017-07-04 19:43:25 +1000

    work in progress

---
+++ dependencies.R
@@ -0,0 +1,24 @@
+
+# check that package dependencies are installed.
+# if any dependencies are missing then attempt to install them.
+install_dependencies <- function(deps=c(), repos=c("https://cran.revolutionanalytics.com")) {
+  missing <- deps[!(deps %in% installed.packages()[,"Package"])]
+  if (length(missing) > 0) {
+    sapply(missing, function(m) {
+      print(paste("Installing missing package", m))
+      install.packages(m, repos=repos)
+    })
+  }
+}
+
+# Load supplied dependency list.
+require_dependencies <- function(deps=c()) {
+  sapply(deps, function(p) require(p, character.only=TRUE))
+}
+
+deps <- c("devtools","MASS", "MVN", "scatterD3", "crosstalk")
+
+install_dependencies (deps)
+
+devtools::install_github("bwlewis/rthreejs")
+
--- prepare_svd_document_model.R
+++ prepare_svd_document_model.R
@@ -28,6 +28,13 @@ prepare_document_model <- function(inputCsv, data_cols, id_cols, outputBase, tex
   words <- unlist(lines_list)
   unique_words <- sort(unique(words))
   
+  # Calculate the column means and sd 
+  column_means <- apply(X, 2, mean)
+  column_sd <- apply(X, 2, sd)
+  
+  # Scale the data set.
+  X <- scale(X)
+  
   searchSpace <- svd(X)
   
   saveRDS(data, file=paste(outputBase, "documents_data.RData", sep=""))
@@ -35,6 +42,9 @@ prepare_document_model <- function(inputCsv, data_cols, id_cols, outputBase, tex
   saveRDS(searchSpace, file=paste(outputBase, "search_space.RData", sep=""))
   saveRDS(doc_term_mat, file=paste(outputBase, "document_term_mat.RData", sep=""))
   saveRDS(term_counts, file=paste(outputBase, "term_counts.RData", sep=""))
+  saveRDS(column_means, file=paste(outputBase, "doc_mat_column_means.RData", sep=""))
+  saveRDS(column_sd, file=paste(outputBase, "doc_mat_column_sdev.RData", sep=""))
+  
 }
 
 load_term_counts <- function(outputBase) {
@@ -47,6 +57,14 @@ load_document_data <- function(outputBase) {
 
 load_document_term_mat <- function(outputBase) {
   readRDS(paste(outputBase, "document_term_mat.RData", sep=""))
+}
+
+load_document_term_column_means <- function(outputBase) {
+  readRDS(paste(outputBase, "doc_mat_column_means.RData", sep=""))
+}
+
+load_document_term_column_sdev <- function(outputBase) {
+  readRDS(paste(outputBase, "doc_mat_column_sdev.RData", sep=""))
 }
 
 load_unique_words <- function(outputBase) {
